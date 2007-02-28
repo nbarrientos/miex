dde8fc933ba93c878438710902a21f7898da9e0d
Nacho Barrientos <nacho@criptonita.com>
2007-02-28 22:12:41 +0000

    final? XML schema and a new script to transform XML collections
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@27 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

---
+++ miex/share/utils/fixxml.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This scripts creates a valid XML file from non-well-formed
+# XML documents (provided by IR group).
+
+if [ ! $# -eq 1 ]; then
+	echo "Usage: $0 inputFile";
+	exit -1;
+fi
+
+if [ ! -f $1 ]; then
+  echo "ERROR: Input file not found or not readable.";
+  exit -1;
+fi
+
+echo "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>" > $1.new
+echo "<COLLECTION>" >> $1.new
+sed -e 's/DOCTRAIN/DOC TRAIN="1"/g' -e 's/DOCTEST/DOC TEST="1"/g' $1 >> $1.new
+echo "</COLLECTION>" >> $1.new
