0275523143dcdedb92d6798628406c82e9d30826
Nacho Barrientos <nacho@criptonita.com>
2007-02-28 22:26:12 +0000

    minor
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@29 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/miex.sh
+++ miex/miex.sh
@@ -6,9 +6,9 @@
 JAVA=java
 CLASSPATH='lib/miex.jar:lib/JSAP-2.1.jar:lib/sax2.jar:lib/stanford-parser-2006-06-11.jar'
 
-if [ ! -x $JAVA ]; then
-	echo "J2SE 1.5.0 JRE required, set \$JAVA variable properly, check permissions and try again, exiting..."
-	exit -1
-fi
+#if [ ! -x $JAVA ]; then
+#	echo "J2SE 1.5.0 JRE required, set \$JAVA variable properly, check permissions and try again, exiting..."
+#	exit -1
+#fi
 
 $JAVA -cp $CLASSPATH es.uniovi.aic.miex.run.Miex $@
