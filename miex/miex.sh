9bf61494f06dc736b94615eefb10200eb48f22b2
Nacho Barrientos <nacho@criptonita.com>
2007-04-05 12:12:01 +0000

    Moving from Make to Ant
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@81 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/miex.sh
+++ miex/miex.sh
@@ -4,7 +4,7 @@
 # Nacho Barrientos Arias <nacho@criptonita.com>
 
 JAVA=java
-CLASSPATH='lib/miex.jar:lib/JSAP-2.1.jar:lib/sax2.jar:lib/stanford-parser-2006-06-11.jar:lib/mysql-connector-java-5.0.5-bin.jar'
+CLASSPATH='build/lib/miex.jar:lib/JSAP-2.1.jar:lib/sax2.jar:lib/stanford-parser-2006-06-11.jar:lib/mysql-connector-java-5.0.5-bin.jar'
 
 #if [ ! -x $JAVA ]; then
 #	echo "J2SE 1.5.0 JRE required, set \$JAVA variable properly, check permissions and try again, exiting..."
