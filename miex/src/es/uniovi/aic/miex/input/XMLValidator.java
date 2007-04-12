eedfd8f2866ab7014f9f8d5b9221a6f0a8678c84
Nacho Barrientos <nacho@criptonita.com>
2007-04-12 21:43:49 +0000

    refact!
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@94 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/src/es/uniovi/aic/miex/input/XMLValidator.java
+++ miex/src/es/uniovi/aic/miex/input/XMLValidator.java
@@ -11,37 +11,33 @@ public class XMLValidator
 {
 		public XMLValidator(String SchemaURI)
 		{
-			XMLSchemaURI = new String(SchemaURI);
+			// 1. Lookup a factory for the W3C XML Schema language
+			SchemaFactory factory =  SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        
+			// 2. Compile the schema. 
+			File schemaLocation = new File(SchemaURI);
+			Schema schema = null;
+
+			try
+			{
+				schema = factory.newSchema(schemaLocation);
+			}
+			catch(Exception e)
+			{
+				System.err.println(e.toString());
+				System.exit(-1);
+			}
+    
+      // 3. Get a validator from the schema.
+			validator = schema.newValidator();
 		}
 
     public boolean validate(File targetFileURI)
-		{
-        // 1. Lookup a factory for the W3C XML Schema language
-        SchemaFactory factory =  SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        
-        // 2. Compile the schema. 
-        // Here the schema is loaded from a java.io.File, but you could use 
-        // a java.net.URL or a javax.xml.transform.Source instead.
-				File schemaLocation = new File(XMLSchemaURI);
-				Schema schema = null;
-
-				try
-				{
-        	schema = factory.newSchema(schemaLocation);
-				}
-				catch(Exception e)
-				{
-					System.err.println(e.toString());
-					return false;
-				}
-    
-        // 3. Get a validator from the schema.
-        Validator validator = schema.newValidator();
-
-        // 4. Parse the document you want to check.
+    {
+        // Parse the document you want to check.
         Source source = new StreamSource(targetFileURI);
 
-        // 5. Check the document
+        // Check the document
         try 
 				{
             validator.validate(source);
@@ -55,6 +51,5 @@ public class XMLValidator
         
     }
 
-		private String XMLSchemaURI;
-
+		private Validator validator;
 }
