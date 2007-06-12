ababe9368ebb9f0cdfec9157a69de1dae399673d
Nacho Barrientos <nacho@criptonita.com>
2007-06-12 09:37:48 +0000

    some jars stripped from distribution branch
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@121 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/miex.sh
+++ miex/miex.sh
@@ -4,7 +4,8 @@
 # Nacho Barrientos Arias <nacho@criptonita.com>
 
 JAVA=java
-CLASSPATH='build/lib/miex.jar:lib/JSAP-2.1.jar:lib/sax2.jar:lib/stanford-parser-2006-06-11.jar:lib/mysql-connector-java-5.0.5-bin.jar'
+CBASEDIR=/usr/lib/java
+CLASSPATH=build/lib/miex.jar:$CBASEDIR/JSAP-2.1.jar:$CBASEDIR/sax2.jar:$CBASEDIR/stanford-parser-2006-06-11.jar:$CBASEDIR/mysql-connector-java-5.0.5-bin.jar
 
 #if [ ! -x $JAVA ]; then
 #	echo "J2SE 1.5.0 JRE required, set \$JAVA variable properly, check permissions and try again, exiting..."
