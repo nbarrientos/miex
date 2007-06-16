de0778ea2050543fe608eb793b3c7e43c841a48d
Nacho Barrientos <nacho@criptonita.com>
2007-06-16 13:15:01 +0000

    added install rules
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@141 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/miex.sh
+++ miex/miex.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-# Small Bash launcher for MIEX
+# Small launcher for MIEX
 #
 # Nacho Barrientos Arias <nacho@criptonita.com>
 
 JAVA=java
 CBASEDIR=/usr/lib/java
-CLASSPATH=build/lib/miex.jar:$CBASEDIR/JSAP-2.1.jar:$CBASEDIR/sax2.jar:$CBASEDIR/stanford-parser-2006-06-11.jar:$CBASEDIR/mysql-connector-java-5.0.5-bin.jar
 
-#if [ ! -x $JAVA ]; then
-#	echo "J2SE 1.5.0 JRE required, set \$JAVA variable properly, check permissions and try again, exiting..."
-#	exit -1
-#fi
+if [ -z `/usr/bin/which java` ]; then
+  echo "J2SE 1.5.0 (or higher) JRE is not in PATH, exiting..."
+  exit -1
+fi
+
+if [ -f build/lib/miex.jar ]; then
+	MIEXJAR=build/lib/miex.jar
+else
+	MIEXJAR=/usr/lib/miex/miex.jar
+fi
+
+CLASSPATH=$MIEXJAR:$CBASEDIR/JSAP-2.1.jar:$CBASEDIR/sax2.jar:$CBASEDIR/stanford-parser-2006-06-11.jar:$CBASEDIR/mysql-connector-java-5.0.5-bin.jar
 
 $JAVA -Xmx1024M -cp $CLASSPATH es.uniovi.aic.miex.run.Miex $@
