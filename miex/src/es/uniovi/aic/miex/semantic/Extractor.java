80c3557f719f2773e3408ddd85856153fd1d05bd
Nacho Barrientos <nacho@criptonita.com>
2007-06-28 18:37:08 +0000

    significant commit - refactoring the method(s) which navigates through the parse tree
    git-svn-id: https://miex.svn.sourceforge.net/svnroot/miex/trunk@213 a2f631d4-a627-0410-a9f9-a0524f6e3c5b

--- miex/src/es/uniovi/aic/miex/semantic/Extractor.java
+++ miex/src/es/uniovi/aic/miex/semantic/Extractor.java
@@ -50,7 +50,6 @@ public class Extractor
 	public void load()
 	{
 		lp = new LexicalizedParser(grammarName); // TODO: Check now if lp is null
-//		lp.setOptionFlags(new String[]{"-noRecoveryTagging"});
 	}
 
 	/** 
@@ -107,88 +106,86 @@ public class Extractor
 		return (ArrayList<TypedDependency>)gs.getDeps(false);
 	}
 
-	/* taggedYield() returns a Sentence object, which inheritates from
-	   java.util.ArrayList<HasWord> so I'm sure it is an 
-     ArrayList<TaggedWord> */
-	@SuppressWarnings("unchecked")
-	
 	/** 
 	 * Gets the grammatical properties for each word in the sentence 
 	 * 
-	 * @param sentence The sentence to process
+	 * @param sentence The sentence to analyse
 	 * @return The result of this process 
 	 */
 	public ArrayList<ExtendedTaggedWord> getProperties(List sentence)
 	throws Exception
 	{
+		// IMPORTANT: Refreshing the parse with the current sentence
 		letsGetParseReady(sentence);
 
-	  List sent = myTaggedYield(parse);
-	
-		return (ArrayList<ExtendedTaggedWord>)sent;
+		return myTaggedYield();
 	}
 
+  /**
+   * Gets a special TaggedYield with phrase and clause level tags in
+   * addiction to the word level tags from the CURRENT stored parse
+   * tree
+   *
+   * @param t The target object to store the captured information
+   * @return The parsed information from THE WHOLE parse tree
+   */
+  private ArrayList<ExtendedTaggedWord> myTaggedYield()
+  {
+    ArrayList<ExtendedTaggedWord> blankTarget = new ArrayList<ExtendedTaggedWord>();
+
+    myTaggedYield(parse,blankTarget,new ArrayList<String>());
+
+    return blankTarget;
+  }
+
 	/** 
-	 * Recursively navigates the parse tree storing all the labels 
+	 * Recursively navigates through the parse tree storing all the labels 
 	 * 
 	 * @param t The tree to parse 
 	 * @param ty Information captured wrt the sentence until now
 	 * @param phrase Phrase tags collected until now
-	 * @return The tagged yield we were looking for 
 	 */
-	private List myTaggedYield(Tree t, Sentence ty, ArrayList<String> phrase)
+	private void myTaggedYield(Tree t, ArrayList<ExtendedTaggedWord> ty, ArrayList<String> phrase)
 	{
-		Tree[] kids = t.children();
-
-    if (kids.length == 1 && kids[0].isLeaf()) // t's root is a)
-    {
-      ExtendedTaggedWord stw = new ExtendedTaggedWord(kids[0].label().value(), t.label().value(), phrase);
-
-      ty.add(stw);
-    }
-    else
-    {
-      for (int i = 0; i < kids.length; i++)
-      {
-        if(kids[i].isPhrasal()) // t's root is b)
-        {
-					ArrayList<String> newphrase = new ArrayList<String>(phrase);
-					newphrase.add(kids[i].label().value());
-					myTaggedYield(kids[i],ty,newphrase);
-        }
-				else // c) t's root is c)
-					myTaggedYield(kids[i],ty,phrase);						
-      }
-    }
-    return ty;
-  }
 
   /*
-	 * Example:
-  
+   * Example:
+
                        ROOT
                         |
      ---- b -->         S
                       /   \
      ---- c -->     NP     VP
-	      				   / | \    \
+                   / | \    \
      ---- a -->  DT NNP ...  ...
                  |   |
                 The cup
-
+   *
    */
 
-	/** 
-	 * Gets a special TaggedYield with phrase and clause level tags in
-	 * addiction to the word level tags 
-	 * 
-	 * @param t A tree representing the parse of a sentence 
-	 * @return The requested information 
-	 */
-	private Sentence myTaggedYield(Tree t)
-	{
-		return (Sentence) myTaggedYield(t,new Sentence(),new ArrayList<String>());
-	}
+		Tree[] kids = t.children();
+
+		// We've got a new word (t's root is something like a)), all the recursive calls leads here
+    if (kids.length == 1 && kids[0].isLeaf()) 
+    {
+      ExtendedTaggedWord stw = new ExtendedTaggedWord(kids[0].label().value(), t.label().value(), phrase);
+      ty.add(stw);
+    }
+    else // Recursively navigating through all the non-final nodes
+    {
+      for (int i=0; i < kids.length; i++)
+      {
+        if(kids[i].isPhrasal()) // t's root is something like b)
+        {
+					ArrayList<String> newphrase = new ArrayList<String>(phrase);
+					newphrase.add(kids[i].label().value()); // new phrase tag found in the path
+					myTaggedYield(kids[i],ty,newphrase);
+        }
+				else // t's root is something like c)
+					myTaggedYield(kids[i],ty,phrase);						
+      }
+    }
+  }
 
 	private LexicalizedParser lp;
 
@@ -197,5 +194,4 @@ public class Extractor
 	private String grammarName;
 
 	private List lastParsedSentence;
-
 }
